9c9c7bb02abbc977e3f0fa172272c80bc002a4e8
OluwaseyiO <oluwaseyi_oluwaleimu@ultimatesoftware.com>
2020-09-27 18:22:12 -0400

    setup the project file structure!

---
+++ README.md
@@ -0,0 +1,13 @@
+# Opensource-Project-Finder
+
+
+## Get started 
+
+
+use `yarn dev` to run the front and backend
+
+
+use `yarn client` or `yarn server` to run their respective folders
+
+
+:)
---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv/config');
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+app.get('/hi', (req,res) => {
+    res.send('Hi!');
+    console.log('hello');
+});
+
+app.get('*', (req,res) => {
+    res.send('Hello World!!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
