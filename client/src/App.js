52300230c806db2b18fb9378ca5c7276d864613f
Allan Lago <allanlago1@gmail.com>
2020-10-01 19:40:30 -0400

    Add nodemon depedency and basic form for prototyping with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  fetch('/hi').then(res => console.log(res));
+  const [languageOption, setLang] = useState("java");
+
+  const search = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.value);
+    const formRes = JSON.stringify({ language: languageOption });
+
+    fetch("/form", {
+      method: "POST",
+      headers: {
+        Accept: "applications/json",
+        "Content-type": "application/json",
+      },
+      body: formRes,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const handleSelectChange = (e) => {
+    setLang(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form method="post" onSubmit={search} className="lang-form">
+          <select name="languages" onChange={handleSelectChange}>
+            <option value="java">Java</option>
+            <option value="python">Python</option>
+            <option value="reactjs">React</option>
+            <option value="c++">C++</option>
+          </select>
+          <button
+            className="submit-button"
+            type="submit"
+            // disabled={typeof languageOption === "undefined"}
+          >
+            Search
+          </button>
+        </form>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
-require('dotenv/config');
-const express = require('express');
+require("dotenv/config");
+const express = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 //Routes
-app.get('/hi', (req,res) => {
-    res.send('Hi!');
-    console.log('hello');
+app.get("/hi", (req, res) => {
+  res.send("Hi!");
+  console.log("hello");
 });
 
-app.get('*', (req,res) => {
-    res.send('Hello World!!');
+app.get("*", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.post("/form", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
