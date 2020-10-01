1785ed9bab82445a979eb119eb742c7b466049aa
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-10-01 18:43:04 -0400

    octokit is functioning, now to make some endpoints for the form!

--- server/index.js
+++ server/index.js
@@ -4,15 +4,34 @@ const port = process.env.PORT || 5000;
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { Octokit } = require("@octokit/rest");
 
 app.use(bodyParser.json());
 app.use(cors());
 
-//Routes
-app.get('/hi', (req,res) => {
-    res.send('Hi!');
-    console.log('hello');
+//octokit (ill move this into a new file once I get how it works :)
+const octoKit = new Octokit({
+
+    userAgent: "OpenSourceProjectFind v.1",
+
 });
+rout
+//searching functionality
+const octoTest = async () =>{
+    let search = "";
+    try{
+        search = await octoKit.search.repos({
+            q:"tetris"
+        });
+    }
+    catch(err){
+        console.log('hello');
+        console.log(err);
+    }
+    console.log(search.data.items[0]);
+};
+octoTest();
+//Routes
 
 app.get('*', (req,res) => {
     res.send('Hello World!!');
